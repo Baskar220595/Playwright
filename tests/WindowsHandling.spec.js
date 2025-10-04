020d56f6fb82157486aae14f1e66efe92e0702c6
Baskar220595 <baskar.ramu.rs@gmail.com>
2025-10-04 13:29:17 +0530

    PlayWright

---
+++ tests/WindowsHandling.spec.js
@@ -0,0 +1,15 @@
+import { expect, test } from "@playwright/test";
+test("Windows Handling", async({browser})=>{
+    
+    const context = await browser.newContext()
+    const page = await context.newPage();
+    await page.goto("https://www.amazon.in/")
+    
+    const amazonHomePage = await page.locator("//input[@id='twotabsearchtextbox']");
+    await amazonHomePage.fill ("iphone17")
+    const submitBtn = await page.locator("//input[@id='nav-search-submit-button']");
+    await submitBtn.click();
+
+    
+    })
+
